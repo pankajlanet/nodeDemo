97c02305cd9214cb308a6994e005e8f01080797a
anish <anis@somethignh.com>
2021-10-01 09:36:06 +0530

    changes in mongoose db

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,5 +3,5 @@ const mongoose = require('mongoose')
 mongoose.connect('mongodb://127.0.0.1:27017/bike-register-api' ).then( ()=> {
     console.log("Connection established")
 }).catch((e) => {
-    console.log("some error occured" ,e);
+    console.log("some error occured in db" ,e);
 })
